f10698f096051ccf8ed82ecd3e94b500e7d4adca
Cammie Reel <camreel@iu.edu>
2025-02-28 15:25:19 -0500

    Declared variables

---
+++ homework-5/bookstore.js
@@ -0,0 +1,7 @@
+const booksList = document.getElementById("books");
+const cartList = document.getElementById("cart");
+const removeButton = document.getElementById("remove-book");
+const noBooks = document.getElementById("no-books");
+
+const availableBooks = ["Hop on Pop", "Fox in Socks", "The Cat in the Hat", "A Fly Went By"];
+
