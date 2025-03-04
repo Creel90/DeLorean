58247cfe2bcae993945a8da58d6b2da3b9fef3ff
SoLaiu <Oabolade@iu.edu>
2025-03-04 11:32:43 -0500

    Update flowers.js

--- homework-5/flowers.js
+++ homework-5/flowers.js
@@ -1,4 +1,3 @@
-// References to HTML elements
 const flowersList = document.getElementById("flowers"),
       cartList = document.getElementById("cart"),
       noFlowersMessage = document.getElementById("no-flowers"),
@@ -7,23 +6,18 @@ const flowersList = document.getElementById("flowers"),
 const availableFlowers = ["Rose", "Tulip", "Daisy", "Lily"];
 let cart = [];
 
-// Display available flowers
 flowersList.innerHTML = availableFlowers.map(flower => 
     `<li>${flower} <button onclick="addToCart('${flower}')">+</button></li>`
 ).join("");
 
-// Update cart display
 const updateCart = () => {
     cartList.innerHTML = cart.map(item => `<li>${item}</li>`).join(""); // Ensures correct display
     noFlowersMessage.style.display = cart.length ? "none" : "block";
 };
 
-// Add flower to cart
 const addToCart = flower => (cart.push(flower), updateCart());
 
-// Remove last flower from cart
 removeFlowerButton.onclick = () => (cart.pop(), updateCart());
 
-// Initialize cart display
 updateCart();
 
